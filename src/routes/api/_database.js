d86e701d1cd066063cb77a4c2f8fa37172d2927f
sisou <hello@soerenschwert.de>
2020-04-29 09:50:28 +0200

    Add .env file

--- src/routes/api/_database.js
+++ src/routes/api/_database.js
@@ -1,10 +1,11 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv'; dotenv.config();
 
 export const db = mysql.createPool({
-	host: process.env.DB_HOST || 'localhost',
-	database: process.env.DB_DATABASE || 'pool',
-	user: process.env.DB_USER || 'pool_info',
-	password: process.env.DB_PASSWORD || '',
+	host: process.env.DB_HOST,
+	database: process.env.DB_DATABASE,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
 	waitForConnections: true,
 	connectionLimit: 10,
 	queueLimit: 0,
